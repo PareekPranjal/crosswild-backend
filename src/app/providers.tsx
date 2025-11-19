72a2ba110e1f8f09e156824d0676365990ecd1e7
PareekPranjal <prnjlpareek@gmail.com>
2025-11-19 12:57:57 +0530

    commented dark defaultTheme = dark

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -4,6 +4,9 @@ import { ThemeProvider } from "next-themes";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
+    // <ThemeProvider attribute="class" enableSystem={false} defaultTheme="dark">
+    //   {children}
+    // </ThemeProvider>
     <ThemeProvider attribute="class" enableSystem={false} defaultTheme="dark">
       {children}
     </ThemeProvider>
